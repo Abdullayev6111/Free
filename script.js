92dd26fd88b4d84a9c8bd6787a910773bcc752c9
Abdurauf Abdullayev <abdullayevaxmadullo977@gmail.com>
2025-08-28 13:54:44 +0500

    changed

--- script.js
+++ script.js
@@ -1,22 +1,61 @@
-const user = {
-  username: "Abdullayev",
-  password: "11111177",
+// User password
 
-  get getPassword() {
-    return this.password;
+// const user = {
+//   username: "Abdullayev",
+//   password: "11111177",
+
+//   get getPassword() {
+//     return this.password;
+//   },
+
+//   set setPassword(newPassword) {
+//     if (newPassword === this.password) {
+//       console.log("Yangi parol eskisi bilan bir xil! Qayta kiriting.");
+//     } else {
+//       this.password = newPassword;
+//       console.log("Parol yangilandi!");
+//     }
+//   }
+// };
+
+// console.log("Eski parol:", user.getPassword);
+// user.setPassword = "11111177"; 
+// user.setPassword = "12345678"; 
+// console.log("Yangi parol:", user.getPassword);
+
+// Kalkulyator
+
+const calculator = {
+  a: 0,
+  b: 0,
+
+  setValues(x, y) {
+    this.a = x;
+    this.b = y;
   },
 
-  set setPassword(newPassword) {
-    if (newPassword === this.password) {
-      console.log("Yangi parol eskisi bilan bir xil! Qayta kiriting.");
-    } else {
-      this.password = newPassword;
-      console.log("Parol yangilandi!");
+  sum() {
+    return this.a + this.b;
+  },
+
+  subtract() {
+    return this.a - this.b;
+  },
+
+  multiply() {
+    return this.a * this.b;
+  },
+
+  divide() {
+    if (this.b === 0) {
+      return "0 ga bo'lish mumkin emas!";
     }
+    return this.a / this.b;
   }
 };
 
-console.log("Eski parol:", user.getPassword);
-user.setPassword = "11111177"; 
-user.setPassword = "12345678"; 
-console.log("Yangi parol:", user.getPassword);
+calculator.setValues(12, 4);
+console.log("Yig'indisi:", calculator.sum());
+console.log("Ayirmasi:", calculator.subtract());
+console.log("Ko'paytmasi:", calculator.multiply());
+console.log("Bo'linmasi:", calculator.divide());
