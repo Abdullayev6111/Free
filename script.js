a21692e36f8f002d041b84c76a41d652cc28ebc4
Abdurauf Abdullayev <abdullayevaxmadullo977@gmail.com>
2025-09-03 17:27:14 +0500

    changed

--- script.js
+++ script.js
@@ -25,37 +25,37 @@
 
 // Kalkulyator
 
-const calculator = {
-  a: 0,
-  b: 0,
+// const calculator = {
+//   a: 0,
+//   b: 0,
 
-  setValues(x, y) {
-    this.a = x;
-    this.b = y;
-  },
+//   setValues(x, y) {
+//     this.a = x;
+//     this.b = y;
+//   },
 
-  sum() {
-    return this.a + this.b;
-  },
+//   sum() {
+//     return this.a + this.b;
+//   },
 
-  subtract() {
-    return this.a - this.b;
-  },
+//   subtract() {
+//     return this.a - this.b;
+//   },
 
-  multiply() {
-    return this.a * this.b;
-  },
+//   multiply() {
+//     return this.a * this.b;
+//   },
 
-  divide() {
-    if (this.b === 0) {
-      return "0 ga bo'lish mumkin emas!";
-    }
-    return this.a / this.b;
-  }
-};
+//   divide() {
+//     if (this.b === 0) {
+//       return "0 ga bo'lish mumkin emas!";
+//     }
+//     return this.a / this.b;
+//   }
+// };
 
-calculator.setValues(12, 4);
-console.log("Yig'indisi:", calculator.sum());
-console.log("Ayirmasi:", calculator.subtract());
-console.log("Ko'paytmasi:", calculator.multiply());
-console.log("Bo'linmasi:", calculator.divide());
+// calculator.setValues(12, 4);
+// console.log("Yig'indisi:", calculator.sum());
+// console.log("Ayirmasi:", calculator.subtract());
+// console.log("Ko'paytmasi:", calculator.multiply());
+// console.log("Bo'linmasi:", calculator.divide());
